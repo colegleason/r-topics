c002f2487c283e11dbd5dd4f2e02a18508049bda
Ciara Proctor <ciarasproctor@gmail.com>
2013-02-24 00:56:21 -0600

    Added a script to grab data from subreddits

---
+++ GetSubreddits.py
@@ -0,0 +1,47 @@
+import sys, os
+
+sys.path.append(os.getcwd() + '/reddiwrap')
+
+print(sys.path)
+
+from ReddiWrap import ReddiWrap
+from yaml import dump
+
+reddit = ReddiWrap()
+
+USERNAME = 'r_topics'
+PASSWORD = ''
+SUB = 'topics'
+
+reddit.load_cookies('cookies.txt')
+
+if not reddit.logged_in or reddit.user.lower() != USERNAME.lower():
+	print('logging into %s' % USERNAME)
+	login = reddit.login(user=USERNAME, password=PASSWORD)
+	if login != 0:
+		print('unable to log in: %d' % login)
+		print('remember to change USERNAME and PASSWORD')
+		exit(1)
+	reddit.save_cookies('cookies.txt')
+print('logged in as %s' % reddit.user)
+
+ 
+posts = reddit.get('/r/%s' % SUB)
+print('getting posts in subreddit /r/%s' % SUB)
+while reddit.has_next():
+	posts += reddit.get_next()
+print('number of posts in /r/%s: %d' % (SUB, len(posts)))
+
+
+subreddit = {}
+
+#each post is a dictionary, contains title, is_self, selftext, comments, num_comments
+for i in range(len(posts)):
+	reddit.fetch_comments(posts[i])
+	subreddit[i] = posts[i]
+
+print dump(subreddit)
+
+yamlfile = open(SUB + '.yaml', 'w')
+dump(subreddit, yamlfile)
+yamlfile.close()
