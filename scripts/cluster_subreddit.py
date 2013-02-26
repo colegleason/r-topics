6716ba3f838ca63e201a40cf48b23c810c199aae
Cole Gleason <colewashere@gmail.com>
2013-02-25 23:58:27 -0600

    moar

--- scripts/cluster_subreddit.py
+++ scripts/cluster_subreddit.py
@@ -1,15 +1,11 @@
 #! /usr/bin/env python
 
-if len(sys.argv) != 3:
-	print 'usage: ./cluster_subreddit carrot_batch_file subreddit_dir'
-	exit(-1)
+# must be in carrot dir and expects data to be in sibling dir
 
 import glob
 import subprocess
 #Get files that need to be clustered
-files = glob.glob(argv[2]+'*.xml')
+files = glob.glob('../data/reddit_xml/*.xml')
 #Run clustering on file, write output in JSON format to ../data/clusters
 for f in files:
-	subprocess.call("./"+argv[1]+" %s -o ../data/clusters -f JSON -d -t" % f, shell=True)
-
-
+	subprocess.call("./batch.sh %s -o ../data/clusters/ -f JSON -d -t" % f, shell=True)
